acfd22dfce8f44ecf082dbdc42a89488f7ba1a8f
Marco Mazzai <mazzai.marco@gmail.com>
2023-05-29 22:06:37 +0200

    - add Heading component

---
+++ app/components/presentational/Heading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface HeadingProps {
+  level: number,
+  label: string
+}
+
+const Heading: React.FC<HeadingProps> = ({level, label}) => {
+  const HeadingTag = `h${level}`;
+
+  return React.createElement(HeadingTag, null, label);
+}
+
+export default Heading
