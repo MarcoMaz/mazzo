40799d35d3a4cd358af3da1229547c0150d95248
Marco Mazzai <mazzai.marco@gmail.com>
2023-05-30 23:55:22 +0200

    ( hi ): destructure component

--- app/components/sections/Hi/Hi.tsx
+++ app/components/sections/Hi/Hi.tsx
@@ -6,21 +6,24 @@ import Image from "next/image";
 import ParagraphGroup from "../../presentational/ParagraphGroup";
 
 const Hi: React.FC = () => {
+  const {
+    headline,
+    image: { url, alt },
+    paragraphs,
+  } = data.hi;
+
   return (
     <>
-      <Heading dataCy="hi-heading" level={1} label={data.hi.headline} />
+      <Heading dataCy="hi-heading" level={1} label={headline} />
       <Image
         data-cy="hi-image"
-        src={data.hi.image.url}
-        alt={data.hi.image.alt}
+        src={url}
+        alt={alt}
         width={200}
         height={200}
         priority
       />
-      <ParagraphGroup
-        dataCy="hi-paragraph-group"
-        paragraphs={data.hi.paragraphs}
-      />
+      <ParagraphGroup dataCy="hi-paragraph-group" paragraphs={paragraphs} />
     </>
   );
 };
