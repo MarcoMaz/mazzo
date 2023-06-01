9dd3bc6aae3d8c98971fd299c242ac93a0dc1e7f
Marco Mazzai <mazzai.marco@gmail.com>
2023-06-02 00:53:08 +0200

    ( cards ): add tests

--- app/components/sections/SelectedWorks/SelectedWorks.spec.cy.js
+++ app/components/sections/SelectedWorks/SelectedWorks.spec.cy.js
@@ -1,6 +1,12 @@
 import SelectedWorks from "./SelectedWorks";
 
 const SELECTED_WORKS_HEADING_SELECTOR = "[data-cy='selected-works-heading']";
+const SELECTED_WORKS_CARDS_SELECTOR = "[data-cy='selected-works-cards']";
+
+const SELECTED_WORKS_CARD_1_SELECTOR = "[data-cy='RenatoDoList']";
+const SELECTED_WORKS_CARD_2_SELECTOR = "[data-cy='Metronome']";
+const SELECTED_WORKS_CARD_3_SELECTOR = "[data-cy='Picker']";
+
 const SELECTED_WORKS_NDA_HEADING_SELECTOR =
   "[data-cy='selected-works-nda-heading']";
 const SELECTED_WORKS_NDA_IMAGES_SELECTOR =
@@ -23,6 +29,181 @@ describe("Headline", () => {
       "have.text",
       "Selected works"
     );
+  });
+});
+
+describe("Cards", () => {
+  beforeEach(() => {
+    cy.mount(<SelectedWorks />);
+  });
+
+  it("should exists", () => {
+    cy.get(SELECTED_WORKS_CARDS_SELECTOR).should("exist");
+  });
+
+  it("should have 3 div cards", () => {
+    cy.get(SELECTED_WORKS_CARDS_SELECTOR)
+      .children("div")
+      .should("have.length", 3);
+  });
+
+  describe("First Card", () => {
+    it("should have the correct headline", () => {
+      cy.get(SELECTED_WORKS_CARD_1_SELECTOR)
+        .find(":first-child")
+        .should("have.text", "RenatoDoList");
+    });
+
+    it("should have the correct subheadline", () => {
+      cy.get(SELECTED_WORKS_CARD_1_SELECTOR)
+        .find(":nth-child(2)")
+        .should("have.text", "PWA");
+    });
+
+    it("should have the correct description", () => {
+      cy.get(SELECTED_WORKS_CARD_1_SELECTOR)
+        .find(":nth-child(3)")
+        .should(
+          "have.text",
+          "Basic metronome with tempo and time signature customisation."
+        );
+    });
+
+    it("should have the correct first chip", () => {
+      cy.get(SELECTED_WORKS_CARD_1_SELECTOR)
+        .find(":nth-child(4)")
+        .should("have.text", "JS");
+    });
+
+    it("should have the correct second chip", () => {
+      cy.get(SELECTED_WORKS_CARD_1_SELECTOR)
+        .find(":nth-child(5)")
+        .should("have.text", "React");
+    });
+
+    it("should have the correct third chip", () => {
+      cy.get(SELECTED_WORKS_CARD_1_SELECTOR)
+        .find(":nth-child(6)")
+        .should("have.text", "HTML");
+    });
+
+    describe("CTA", () => {
+      it("should have the correct label", () => {
+        cy.get(SELECTED_WORKS_CARD_1_SELECTOR)
+          .find(":nth-child(7)")
+          .should("have.text", "Check it out");
+      });
+
+      it("should have the correct url", () => {
+        cy.get(SELECTED_WORKS_CARD_1_SELECTOR)
+          .find(":nth-child(7)")
+          .should("have.attr", "href", "#");
+      });
+    });
+  });
+
+  describe("Second Card", () => {
+    it("should have the correct headline", () => {
+      cy.get(SELECTED_WORKS_CARD_2_SELECTOR)
+        .find(":first-child")
+        .should("have.text", "Metronome");
+    });
+
+    it("should have the correct subheadline", () => {
+      cy.get(SELECTED_WORKS_CARD_2_SELECTOR)
+        .find(":nth-child(2)")
+        .should("have.text", "WebApp");
+    });
+
+    it("should have the correct description", () => {
+      cy.get(SELECTED_WORKS_CARD_2_SELECTOR)
+        .find(":nth-child(3)")
+        .should("have.text", "Something somtehing...");
+    });
+
+    it("should have the correct first chip", () => {
+      cy.get(SELECTED_WORKS_CARD_2_SELECTOR)
+        .find(":nth-child(4)")
+        .should("have.text", "JS");
+    });
+
+    it("should have the correct second chip", () => {
+      cy.get(SELECTED_WORKS_CARD_2_SELECTOR)
+        .find(":nth-child(5)")
+        .should("have.text", "React");
+    });
+
+    it("should have the correct third chip", () => {
+      cy.get(SELECTED_WORKS_CARD_2_SELECTOR)
+        .find(":nth-child(6)")
+        .should("have.text", "HTML");
+    });
+
+    describe("CTA", () => {
+      it("should have the correct label", () => {
+        cy.get(SELECTED_WORKS_CARD_2_SELECTOR)
+          .find(":nth-child(7)")
+          .should("have.text", "Check it out");
+      });
+
+      it("should have the correct url", () => {
+        cy.get(SELECTED_WORKS_CARD_2_SELECTOR)
+          .find(":nth-child(7)")
+          .should("have.attr", "href", "#");
+      });
+    });
+  });
+
+  describe("Third Card", () => {
+    it("should have the correct headline", () => {
+      cy.get(SELECTED_WORKS_CARD_3_SELECTOR)
+        .find(":first-child")
+        .should("have.text", "Picker");
+    });
+
+    it("should have the correct subheadline", () => {
+      cy.get(SELECTED_WORKS_CARD_3_SELECTOR)
+        .find(":nth-child(2)")
+        .should("have.text", "Web Component");
+    });
+
+    it("should have the correct description", () => {
+      cy.get(SELECTED_WORKS_CARD_3_SELECTOR)
+        .find(":nth-child(3)")
+        .should("have.text", "LALALALLALALALAalllala.");
+    });
+
+    it("should have the correct first chip", () => {
+      cy.get(SELECTED_WORKS_CARD_3_SELECTOR)
+        .find(":nth-child(4)")
+        .should("have.text", "JS");
+    });
+
+    it("should have the correct second chip", () => {
+      cy.get(SELECTED_WORKS_CARD_3_SELECTOR)
+        .find(":nth-child(5)")
+        .should("have.text", "React");
+    });
+
+    it("should have the correct third chip", () => {
+      cy.get(SELECTED_WORKS_CARD_3_SELECTOR)
+        .find(":nth-child(6)")
+        .should("have.text", "HTML");
+    });
+
+    describe("CTA", () => {
+      it("should have the correct label", () => {
+        cy.get(SELECTED_WORKS_CARD_3_SELECTOR)
+          .find(":nth-child(7)")
+          .should("have.text", "Check it out");
+      });
+
+      it("should have the correct url", () => {
+        cy.get(SELECTED_WORKS_CARD_3_SELECTOR)
+          .find(":nth-child(7)")
+          .should("have.attr", "href", "#");
+      });
+    });
   });
 });
 
