d0aa7a9495acedcca98833077e05874d0f6558cc
Marco Mazzai <mazzai.marco@gmail.com>
2023-06-03 22:30:49 +0200

    ( card ): fix styling

--- app/components/organisms/CardGroup/CardGroup.tsx
+++ app/components/organisms/CardGroup/CardGroup.tsx
@@ -10,25 +10,27 @@ interface CardGroupProps {
 
 const CardGroup: React.FC<CardGroupProps> = ({ cards, dataCy }) => {
   return (
-    <div data-cy={dataCy} className={styles.container}>
-      {cards.map(
-        (
-          { headline, subheadline, description, chips, CTA: { url, label } },
-          index
-        ) => (
-          <Card
-            headline={headline}
-            subheadline={subheadline}
-            description={description}
-            chips={chips}
-            CTA={{ url, label }}
-            dataCy={headline}
-            key={index}
-          />
-        )
-      )}
+    <>
+      <div data-cy={dataCy} className={styles.container}>
+        {cards.map(
+          (
+            { headline, subheadline, description, chips, CTA: { url, label } },
+            index
+          ) => (
+            <Card
+              headline={headline}
+              subheadline={subheadline}
+              description={description}
+              chips={chips}
+              CTA={{ url, label }}
+              dataCy={headline}
+              key={index}
+            />
+          )
+        )}
+      </div>
       <DotGroup dots={cards} dataCy="Dots" />
-    </div>
+    </>
   );
 };
 
