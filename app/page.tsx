c3b1bb7486105c35929c17685c0234c741ac898c
Marco Mazzai <mazzai.marco@gmail.com>
2023-06-05 23:28:46 +0200

    - clean up

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,12 @@ import Hi from "./components/templates/Hi/Hi";
 import SelectedWorks from "./components/templates/SelectedWorks/SelectedWorks";
 import WriteMe from "./components/templates/WriteMe/WriteMe";
 
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
     <>
       <Hi />
       <nav></nav>
-      <main className={styles.main}>
+      <main>
         <SelectedWorks />
         {/* <Posts /> */}
       </main>
