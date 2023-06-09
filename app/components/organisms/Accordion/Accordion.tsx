f9f6c2fb5787936ac5c37b00c1ec65fde5f4c7d7
Marco Mazzai <mazzai.marco@gmail.com>
2023-06-09 23:22:18 +0200

    ( accordion ): fix logic on load

--- app/components/organisms/Accordion/Accordion.tsx
+++ app/components/organisms/Accordion/Accordion.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import styles from "./Accordion.module.css";
 
 import AccordionHeader from "../../molecules/AccordionHeader/AccordionHeader";
 import AccordionPanel from "../../molecules/AccordionPanel/AccordionPanel";
 
 import { CardProps } from "../../molecules/Card/Card";
+import { useState } from "react";
+import React from "react";
 
 interface AccordionProps {
   dataCy: string;
@@ -12,6 +16,8 @@ interface AccordionProps {
 }
 
 const Accordion: React.FC<AccordionProps> = ({ cards, dataCy, className }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   const combinedClassName = className
     ? `${styles.container} ${className}`
     : styles.container;
@@ -23,23 +29,27 @@ const Accordion: React.FC<AccordionProps> = ({ cards, dataCy, className }) => {
           { headline, subheadline, description, chips, CTA: { url, label } },
           index
         ) => (
-          <>
-            <AccordionHeader
-              headline={headline}
-              index={index}
-              dataCy={`selected-works-accordion-group-${headline}`}
-            />
-            <AccordionPanel
-              panelId={`sect${index}`}
-              dataCy={`selected-works-accordion-panel-${headline}`}
-              buttonId={`accordion${index}id`}
-              headline={headline}
-              subheadline={subheadline}
-              description={description}
-              chips={chips}
-              CTA={{ url, label }}
-            />
-          </>
+          <React.Fragment key={index}>
+            {index !== activeIndex && (
+              <AccordionHeader
+                headline={headline}
+                index={index}
+                dataCy={`selected-works-accordion-group-${headline}`}
+              />
+            )}
+            {index === activeIndex && (
+              <AccordionPanel
+                panelId={`sect${index}`}
+                dataCy={`selected-works-accordion-panel-${headline}`}
+                buttonId={`accordion${index}id`}
+                headline={headline}
+                subheadline={subheadline}
+                description={description}
+                chips={chips}
+                CTA={{ url, label }}
+              />
+            )}
+          </React.Fragment>
         )
       )}
     </div>
