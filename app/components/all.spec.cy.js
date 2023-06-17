fab6eb2d8a2fc987383a57274607299bc21caa42
Marco Mazzai <mazzai.marco@gmail.com>
2023-06-17 23:49:26 +0200

    ( tests ): fix recap file

--- app/components/all.spec.cy.js
+++ app/components/all.spec.cy.js
@@ -1,3 +1,6 @@
 import "./templates/Hi/Hi.spec.cy";
-import "./templates/SelectedWorks/SelectedWorks.spec.cy";
+import "./templates/SelectedWorks/tests/Accordion.spec.cy";
+import "./templates/SelectedWorks/tests/Cards.spec.cy";
+import "./templates/SelectedWorks/tests/NDA.spec.cy";
+import "./templates/SelectedWorks/tests/KnowMore.spec";
 import "./templates/WriteMe/WriteMe.spec.cy";
