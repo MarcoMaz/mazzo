7419d16d3ee6a55cfb9e52456a308f2f3fa21dbb
marcomazzaifitxr <marco.mazzai@fitxr.com>
2024-03-10 21:26:58 +0100

    fix( ChipGroup ): - add story - clean up

---
+++ components/molecules/ChipGroup/ChipGroup.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import ChipGroup from "./ChipGroup";
+
+const meta: Meta<typeof ChipGroup> = {
+  component: ChipGroup,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ChipGroup>;
+
+export const ChipGroupStory: Story = {
+  name: "ChipGroup",
+  render: (props) => <ChipGroup {...props} />,
+  argTypes: {
+    chips: {
+      control: {
+        type: "array",
+      },
+    },
+  },
+  args: {
+    chips: [
+      "Sample Text",
+      "Text",
+      "Label",
+      "Text",
+      "Text",
+      "Label",
+      "Sample Text",
+      "Text",
+      "Label",
+      "Label",
+      "Sample Text",
+      "Sample Text",
+      "Text",
+      "Label",
+      "Text",
+      "Text",
+      "Label",
+      "Sample Text",
+      "Text",
+    ],
+  },
+};
