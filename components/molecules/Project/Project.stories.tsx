556ea35b44871f0b186497ffe68b9ddc38d67203
marcomazzaifitxr <marco.mazzai@fitxr.com>
2024-03-16 00:21:05 +0100

    feat( Poject ): add component

---
+++ components/molecules/Project/Project.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Project from "./Project";
+
+const meta: Meta<typeof Project> = {
+  component: Project,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Project>;
+
+export const ProjectStory: Story = {
+  name: "Project",
+  render: (props) => <Project {...props} />,
+  argTypes: {
+    headline: {
+      control: {
+        type: "text",
+      },
+    },
+  },
+  args: {
+    headline: "Sample Title",
+    subheadline: "Sample Subtitle",
+    description: "Sample Description",
+    chips: ["Chip 1", "Chip 2"],
+    CTA: {
+      url: "#",
+      label: "Read More",
+    },
+  },
+};
---
+++ components/molecules/Project/Project.tsx
@@ -0,0 +1,38 @@
+import "./Project.css";
+
+import Heading from "../../../components/atoms/Heading/Heading";
+import Paragraph from "../Paragraph/Paragraph";
+import ChipGroup from "../ChipGroup/ChipGroup";
+import ExternalLink from "../../../components/atoms/ExternalLink/ExternalLink";
+import { FunctionComponent } from "react";
+
+interface ProjectProps {
+  headline: string;
+  subheadline: string;
+  description: string;
+  chips: string[];
+  CTA: {
+    url: string;
+    label: string;
+  };
+}
+
+const Project: FunctionComponent<ProjectProps> = ({
+  headline,
+  subheadline,
+  description,
+  chips,
+  CTA: { url, label },
+}) => {
+  return (
+    <div className="project">
+      <Heading level={3} label={headline} />
+      <Heading level={4} label={subheadline} />
+      <Paragraph text={description} />
+      <ChipGroup chips={chips} />
+      <ExternalLink url={url} label={label} />
+    </div>
+  );
+};
+
+export default Project;
