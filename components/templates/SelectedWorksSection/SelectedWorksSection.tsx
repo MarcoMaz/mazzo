b69a79447258dc05ba8d7c3f3fa8c9a4bb694854
marcomazzaifitxr <marco.mazzai@fitxr.com>
2024-03-17 23:13:31 +0100

    fix( SelectedWorks ): clean up

--- components/templates/SelectedWorksSection/SelectedWorksSection.tsx
+++ components/templates/SelectedWorksSection/SelectedWorksSection.tsx
@@ -11,13 +11,55 @@ import React from "react";
 import Image from "next/image";
 
 const SelectedWorksSection = () => {
-  const { headline, cards, nda, knowMore } = data.selectedWorks;
+  const { headline, nda, knowMore } = data.selectedWorks;
 
   return (
     <div className="selected-works-section" id="selected-works">
       <Heading level={2} label={headline} />
-      {/* <Accordion cards={cards} />
-      <CardCarousel cards={cards} /> */}
+      <Accordion
+        items={[
+          {
+            buttonId: "accordion-item-1",
+            hasBorderReset: true,
+            headline: "Sample Title 1",
+            children: <>Sample Content 1</>,
+          },
+          {
+            buttonId: "accordion-item-2",
+            hasBorderReset: false,
+            headline: "Sample Title 2",
+            children: <>Sample Content 2</>,
+          },
+          {
+            buttonId: "accordion-item-3",
+            hasBorderReset: false,
+            headline: "Sample Title 3",
+            children: <>Sample Content 3</>,
+          },
+        ]}
+      />
+    <CardCarousel>
+      <>
+        <h1>Heading 1</h1>
+        <h2>SubHeading 1</h2>
+        <p>This is some text</p>
+      </>
+      <>
+        <h1>Heading 2</h1>
+        <h2>SubHeading 2</h2>
+        <p>This is some text</p>
+      </>
+      <>
+        <h1>Heading 3</h1>
+        <h2>SubHeading 3</h2>
+        <p>This is some text</p>
+      </>
+      <>
+        <h1>Heading 4</h1>
+        <h2>SubHeading 4</h2>
+        <p>This is some text</p>
+      </>
+    </CardCarousel>
       <Heading level={3} label={nda.headline} />
       <div data-cy={"selected-works-nda-images"}>
         {nda.images.map(({ src, alt }) => (
