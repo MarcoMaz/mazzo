699c942f5058ccde5927788c52eb0a8faa175b98
Marco Mazzai <mazzai.marco@gmail.com>
2024-09-25 21:16:26 +0200

    feat( carousel ): add next and prev buttons logic

--- components/organisms/NewCarousel/NewCarousel.tsx
+++ components/organisms/NewCarousel/NewCarousel.tsx
@@ -18,7 +18,7 @@ const NewCarouselNavigation: React.FC<NewCarouselNavigationProps> = ({
         <li key={index}>
           <button
             data-slide={index}
-            className={index === activeIndex ? '-full' : ''}
+            className={index === activeIndex ? '-full' : undefined}
             type='button'
           >
             <Circle />
@@ -29,16 +29,24 @@ const NewCarouselNavigation: React.FC<NewCarouselNavigationProps> = ({
   );
 };
 
-const NewCarouselControls = () => {
+interface NewCarouselControlsProps {
+  onNext: () => void;
+  onPrev: () => void;
+}
+
+const NewCarouselControls: React.FC<NewCarouselControlsProps> = ({
+  onNext,
+  onPrev,
+}) => {
   return (
     <ul className='controls'>
       <li>
-        <button type='button' className='btn-prev'>
+        <button type='button' className='btn-prev' onClick={onPrev}>
           <ChevronLeft />
         </button>
       </li>
       <li>
-        <button type='button' className='btn-next'>
+        <button type='button' className='btn-next' onClick={onNext}>
           <ChevronRight />
         </button>
       </li>
@@ -77,7 +85,6 @@ const NewCarousel: React.FC<NewCarouselProps> = ({ items }) => {
         // Calculate the index of the item at the leftmost position
         const newIndex = Math.round(scrollLeft / itemWidth);
         setActiveIndex(newIndex);
-        console.log('Active Index:', newIndex); // Log the active index
       }
     };
 
@@ -93,6 +100,43 @@ const NewCarousel: React.FC<NewCarouselProps> = ({ items }) => {
     };
   }, [items.length]);
 
+  const handleNext = () => {
+    const container = containerRef.current;
+    if (container) {
+      const newIndex = Math.min(activeIndex + 1, items.length - 1);
+
+      const itemWidth = container.scrollWidth / items.length;
+
+      container.scrollTo({
+        left: newIndex * itemWidth,
+        behavior: 'smooth',
+      });
+
+      // Update the activeIndex after a short delay to allow scrolling to complete
+      setTimeout(() => {
+        setActiveIndex(newIndex);
+      }, 500); // Adjust the timeout duration based on your scroll animation duration
+    }
+  };
+
+  const handlePrev = () => {
+    const container = containerRef.current;
+    if (container) {
+      const newIndex = Math.max(activeIndex - 1, 0); // Calculate new index
+      const itemWidth = container.scrollWidth / items.length;
+
+      container.scrollTo({
+        left: newIndex * itemWidth,
+        behavior: 'smooth',
+      });
+
+      // Update the activeIndex after a short delay to allow scrolling to complete
+      setTimeout(() => {
+        setActiveIndex(newIndex);
+      }, 500); // Adjust the timeout duration based on your scroll animation duration
+    }
+  };
+
   return (
     <section className='carousel' aria-label='Recent news'>
       <ul className='cards' ref={containerRef}>
@@ -102,7 +146,7 @@ const NewCarousel: React.FC<NewCarouselProps> = ({ items }) => {
           </NewCarouselItem>
         ))}
       </ul>
-      <NewCarouselControls />
+      <NewCarouselControls onNext={handleNext} onPrev={handlePrev} />
       <NewCarouselNavigation items={items} activeIndex={activeIndex} />
     </section>
   );
