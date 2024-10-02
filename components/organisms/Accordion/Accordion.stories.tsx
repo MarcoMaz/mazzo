1cac02c0604fb9d0c72860583d5d40ce972e7108
Marco Mazzai <mazzai.marco@gmail.com>
2024-10-02 22:11:03 +0200

    fix storybook

--- components/organisms/Accordion/Accordion.stories.tsx
+++ components/organisms/Accordion/Accordion.stories.tsx
@@ -1,8 +1,35 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import Accordion from "./Accordion";
+import type { Meta, StoryObj } from '@storybook/react';
+import Accordion from './Accordion';
 
 const meta: Meta<typeof Accordion> = {
   component: Accordion,
+  argTypes: {
+    items: {
+      control: {
+        type: 'object',
+      },
+    },
+  },
+  args: {
+    items: [
+      {
+        id: '1',
+        headline: 'Sample Title 1',
+        children: <p>Sample Content 1</p>,
+      },
+      {
+        id: '2',
+        headline: 'Sample Title 2',
+        children: <p>Sample Content 2</p>,
+      },
+      {
+        id: '3',
+        headline: 'Sample Title 3',
+        children: <p>Sample Content 3</p>,
+      },
+    ],
+  },
+  render: (props) => <Accordion {...props} />,
 };
 
 export default meta;
@@ -10,32 +37,5 @@ export default meta;
 type Story = StoryObj<typeof Accordion>;
 
 export const AccordionStory: Story = {
-  name: "Accordion",
-  render: (props) => <Accordion {...props}/>,
-  argTypes: {
-    items: {
-      control: {
-        type: "object",
-      },
-    },
-  },
-  args: {
-    items: [
-      {
-        id: "1",
-        headline: "Sample Title 1",
-        children: <p>Sample Content 1</p>,
-      },
-      {
-        id: "2",
-        headline: "Sample Title 2",
-        children: <p>Sample Content 2</p>,
-      },
-      {
-        id: "3",
-        headline: "Sample Title 3",
-        children: <p>Sample Content 3</p>,
-      },
-    ],
-  },
+  name: 'Default',
 };
--- components/organisms/Carousel/Carousel.stories.tsx
+++ components/organisms/Carousel/Carousel.stories.tsx
@@ -3,98 +3,6 @@ import Carousel from './Carousel';
 
 const meta: Meta<typeof Carousel> = {
   component: Carousel,
-};
-
-export default meta;
-
-type Story = StoryObj<typeof Carousel>;
-
-const items = [
-  {
-    id: '1',
-    children: (
-      <article>
-        <h2>Heading 1</h2>
-        <p>This is some text</p>
-        <a href='#'>Link 1</a>
-      </article>
-    ),
-  },
-  {
-    id: '2',
-    children: (
-      <>
-        <h2>Heading 2</h2>
-        <p>This is some text</p>
-        <a href='#'>Link 2</a>
-      </>
-    ),
-  },
-  {
-    id: '3',
-    children: (
-      <>
-        <h2>Heading 3</h2>
-        <p>This is some text</p>
-        <a href='#'>Link 3</a>
-      </>
-    ),
-  },
-  {
-    id: '4',
-    children: (
-      <>
-        <h2>Heading 4</h2>
-        <p>This is some text</p>
-        <a href='#'>Link 4</a>
-      </>
-    ),
-  },
-  {
-    id: '5',
-    children: (
-      <>
-        <h2>Heading 5</h2>
-        <p>This is some text</p>
-        <a href='#'>Link 5</a>
-      </>
-    ),
-  },
-  {
-    id: '6',
-    children: (
-      <>
-        <h2>Heading 6</h2>
-        <p>This is some text</p>
-        <a href='#'>Link 6</a>
-      </>
-    ),
-  },
-  {
-    id: '7',
-    children: (
-      <>
-        <h2>Heading 7</h2>
-        <p>This is some text</p>
-        <a href='#'>Link 7</a>
-      </>
-    ),
-  },
-  {
-    id: '8',
-    children: (
-      <>
-        <h2>Heading 8</h2>
-        <p>This is some text</p>
-        <a href='#'>Link 8</a>
-      </>
-    ),
-  },
-];
-
-export const CarouselStory: Story = {
-  name: 'Carousel',
-  render: (props) => <Carousel {...props} />,
   argTypes: {
     items: {
       control: {
@@ -113,8 +21,98 @@ export const CarouselStory: Story = {
     },
   },
   args: {
-    items: items,
+    items: [
+      {
+        id: '1',
+        children: (
+          <article>
+            <h2>Heading 1</h2>
+            <p>This is some text</p>
+            <a href='#'>Link 1</a>
+          </article>
+        ),
+      },
+      {
+        id: '2',
+        children: (
+          <>
+            <h2>Heading 2</h2>
+            <p>This is some text</p>
+            <a href='#'>Link 2</a>
+          </>
+        ),
+      },
+      {
+        id: '3',
+        children: (
+          <>
+            <h2>Heading 3</h2>
+            <p>This is some text</p>
+            <a href='#'>Link 3</a>
+          </>
+        ),
+      },
+      {
+        id: '4',
+        children: (
+          <>
+            <h2>Heading 4</h2>
+            <p>This is some text</p>
+            <a href='#'>Link 4</a>
+          </>
+        ),
+      },
+      {
+        id: '5',
+        children: (
+          <>
+            <h2>Heading 5</h2>
+            <p>This is some text</p>
+            <a href='#'>Link 5</a>
+          </>
+        ),
+      },
+      {
+        id: '6',
+        children: (
+          <>
+            <h2>Heading 6</h2>
+            <p>This is some text</p>
+            <a href='#'>Link 6</a>
+          </>
+        ),
+      },
+      {
+        id: '7',
+        children: (
+          <>
+            <h2>Heading 7</h2>
+            <p>This is some text</p>
+            <a href='#'>Link 7</a>
+          </>
+        ),
+      },
+      {
+        id: '8',
+        children: (
+          <>
+            <h2>Heading 8</h2>
+            <p>This is some text</p>
+            <a href='#'>Link 8</a>
+          </>
+        ),
+      },
+    ],
     ariaLabelMainTopic: 'Main Topic',
     ariaLabelTopic: 'Topic',
   },
+  render: (props) => <Carousel {...props} />,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Carousel>;
+
+export const CarouselStory: Story = {
+  name: 'Default',
 };
