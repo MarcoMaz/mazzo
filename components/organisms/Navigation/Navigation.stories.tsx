de7e3c0b3b5c08617ca3e8fc588129576171de52
Marco Mazzai <mazzai.marco@gmail.com>
2024-10-02 22:37:37 +0200

    fix( navigation ): fix stories

--- components/organisms/Navigation/Navigation.stories.tsx
+++ components/organisms/Navigation/Navigation.stories.tsx
@@ -1,72 +1,65 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import Navigation from "./Navigation";
+import type { Meta, StoryObj } from '@storybook/react';
+import Navigation from './Navigation';
 
 const meta: Meta<typeof Navigation> = {
   component: Navigation,
+  argTypes: {
+    navLinks: {
+      control: {
+        type: 'object',
+      },
+    },
+  },
+  args: {
+    navLinks: [
+      {
+        url: '#',
+        label: 'Link #1',
+      },
+      {
+        url: '#',
+        label: 'Link #2',
+      },
+      {
+        url: '#',
+        label: 'Link #3',
+      },
+      {
+        url: '#',
+        label: 'Link #4',
+      },
+    ],
+  },
+  render: (props) => <Navigation {...props} />,
 };
 
 export default meta;
 
 type Story = StoryObj<typeof Navigation>;
 
-const baseArgs = {
-  navLinks: [
-    {
-      url: "#",
-      label: "Link #1",
-    },
-    {
-      url: "#",
-      label: "Link #2",
-    },
-    {
-      url: "#",
-      label: "Link #3",
-    },
-    {
-      url: "#",
-      label: "Link #4",
-    },
-  ],
-};
-
-const argTypes = {
-  navLinks: {
-    control: {
-      type: "object",
-    },
-  },
-};
-
 export const NavigationStory: Story = {
-  name: "Navigation (Black Background)",
+  name: 'Default',
   render: (props) => (
-    <div style={{ background: "var(--colorBlack)", height: "70vh" }}>
+    <div style={{ background: 'var(--colorPrimary)', height: '100vh' }}>
       <Navigation {...props} />
     </div>
   ),
-  argTypes,
-  args: baseArgs,
+};
+
+export const NavigationStoryBlackBackground: Story = {
+  name: 'With Black Background',
+  render: (props) => (
+    <div style={{ background: 'var(--colorBlack)', height: '100vh' }}>
+      <Navigation {...props} />
+    </div>
+  ),
 };
 
 export const NavigationStoryWhiteBackground: Story = {
-  name: "Navigation (White Background)",
+  name: 'With White Background',
   render: (props) => (
-    <div style={{ background: "var(--colorWhite)", height: "70vh" }}>
+    <div style={{ background: 'var(--colorWhite)', height: '100vh' }}>
       <Navigation {...props} />
     </div>
   ),
-  argTypes,
-  args: baseArgs,
-};
-
-export const NavigationStoryPrimaryBackground: Story = {
-  name: "Navigation (Primary Background)",
-  render: (props) => (
-    <div style={{ background: "var(--colorPrimary)", height: "70vh" }}>
-      <Navigation {...props} />
-    </div>
-  ),
-  argTypes,
-  args: baseArgs,
 };
