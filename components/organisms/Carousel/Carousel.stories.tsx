70fe09eaadb769ac0d0b41d0cb51d0ff998dbed6
Marco Mazzai <mazzai.marco@gmail.com>
2024-10-04 22:26:54 +0200

    fix( carousel ): fix stories

--- components/organisms/Carousel/Carousel.stories.tsx
+++ components/organisms/Carousel/Carousel.stories.tsx
@@ -24,83 +24,43 @@ const meta: Meta<typeof Carousel> = {
     items: [
       {
         id: '1',
-        children: (
-          <article>
-            <h2>Heading 1</h2>
-            <p>This is some text</p>
-            <a href='#'>Link 1</a>
-          </article>
-        ),
+        children:
+          "<h2>Heading 1</h2><p>This is some text</p><a href='#'>Link 1</a>",
       },
       {
         id: '2',
-        children: (
-          <>
-            <h2>Heading 2</h2>
-            <p>This is some text</p>
-            <a href='#'>Link 2</a>
-          </>
-        ),
+        children:
+          "<h2>Heading 2</h2><p>This is some text</p><a href='#'>Link 2</a>",
       },
       {
         id: '3',
-        children: (
-          <>
-            <h2>Heading 3</h2>
-            <p>This is some text</p>
-            <a href='#'>Link 3</a>
-          </>
-        ),
+        children:
+          "<h2>Heading 3</h2><p>This is some text</p><a href='#'>Link 3</a>",
       },
       {
         id: '4',
-        children: (
-          <>
-            <h2>Heading 4</h2>
-            <p>This is some text</p>
-            <a href='#'>Link 4</a>
-          </>
-        ),
+        children:
+          "<h2>Heading 4</h2><p>This is some text</p><a href='#'>Link 4</a>",
       },
       {
         id: '5',
-        children: (
-          <>
-            <h2>Heading 5</h2>
-            <p>This is some text</p>
-            <a href='#'>Link 5</a>
-          </>
-        ),
+        children:
+          "<h2>Heading 5</h2><p>This is some text</p><a href='#'>Link 5</a>",
       },
       {
         id: '6',
-        children: (
-          <>
-            <h2>Heading 6</h2>
-            <p>This is some text</p>
-            <a href='#'>Link 6</a>
-          </>
-        ),
+        children:
+          "<h2>Heading 6</h2><p>This is some text</p><a href='#'>Link 6</a>",
       },
       {
         id: '7',
-        children: (
-          <>
-            <h2>Heading 7</h2>
-            <p>This is some text</p>
-            <a href='#'>Link 7</a>
-          </>
-        ),
+        children:
+          "<h2>Heading 7</h2><p>This is some text</p><a href='#'>Link 7</a>",
       },
       {
         id: '8',
-        children: (
-          <>
-            <h2>Heading 8</h2>
-            <p>This is some text</p>
-            <a href='#'>Link 8</a>
-          </>
-        ),
+        children:
+          "<h2>Heading 8</h2><p>This is some text</p><a href='#'>Link 8</a>",
       },
     ],
     ariaLabelMainTopic: 'Main Topic',
